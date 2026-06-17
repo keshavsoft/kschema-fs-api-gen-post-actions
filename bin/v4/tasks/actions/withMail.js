54dbb533e5ed5bfbb5f45ac92a2ff54d134f29ac
keshavsoft <nknnkumar@yahoo.com>
2026-06-17 13:50:12 +0530

    4 to test

--- bin/v4/tasks/actions/withMail.js
+++ bin/v4/tasks/actions/withMail.js
@@ -1,17 +1,17 @@
 import path from "path";
 import fixEndpointsJs from "express-fix-any-js";
 
-import { locateSource } from "./PostMethods/WithMail/steps/locateSource.js";
-import { locateDestination } from "./PostMethods/WithMail/steps/locateDestination.js";
+import { locateSource } from "./WithMail/steps/locateSource.js";
+import { locateDestination } from "./WithMail/steps/locateDestination.js";
 import { createFolder } from "../../core/createFolder.js";
 
-import updateEndPointsJs from "./PostMethods/WithMail/steps/updateEndPointsJs.js";
-import createHttpFile from "./PostMethods/WithMail/steps/createHttpFile.js";
+import updateEndPointsJs from "./WithMail/steps/updateEndPointsJs.js";
+import createHttpFile from "./WithMail/steps/createHttpFile.js";
 
-import { announce } from "./PostMethods/WithMail/steps/announce.js";
+import { announce } from "./WithMail/steps/announce.js";
 
-import resolveFolderName from "./PostMethods/WithMail/steps/resolveFolderName.js";
-import actions from "./PostMethods/WithMail/actions.json" with { type: "json" };
+import resolveFolderName from "./WithMail/steps/resolveFolderName.js";
+import actions from "./WithMail/actions.json" with { type: "json" };
 
 const startFunc = async ({ cmd = "", toPath, isAnnounce = true, checkBeforeCreate = true }) => {
 
