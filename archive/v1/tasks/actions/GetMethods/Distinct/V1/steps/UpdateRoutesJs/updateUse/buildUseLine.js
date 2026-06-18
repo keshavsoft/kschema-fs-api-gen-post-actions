cab5757032a26621b261e99a35a1be06d75bbcf7
keshavsoft <nknnkumar@yahoo.com>
2026-06-18 16:22:10 +0530

    del

--- archive/v1/tasks/actions/GetMethods/Distinct/V1/steps/UpdateRoutesJs/updateUse/buildUseLine.js
+++
@@ -1,9 +0,0 @@
-const appOrRouter = "router";
-
-const buildUseLine = ({ inEndpoint, inFuncName }) => {
-    return `${appOrRouter}.get('/${inEndpoint}', (req, res) => ${inFuncName}({ res, inTableName : tableName}));`;
-
-    // return `${appOrRouter}.use("/${inEndpoint}", ${inFuncName});`;
-};
-
-export default buildUseLine;
--- archive/v1/tasks/actions/PostMethods/Alter/template/v4/Service/locaterow.js
+++
@@ -1,7 +0,0 @@
-const LocateRowFunc = ({ inData, inRequestPk }) => {
-    return inData.findIndex(element => {
-        return element.pk === Number(inRequestPk);
-    });
-};
-
-export default LocateRowFunc;
