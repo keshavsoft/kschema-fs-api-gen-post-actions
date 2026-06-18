df7666c721622c34cd75069c07d9e17c1cee8898
keshavsoft <nknnkumar@yahoo.com>
2026-06-18 16:26:22 +0530

    9

--- bin/v9/tasks/actions/withMail.js
+++ bin/v9/tasks/actions/withMail.js
@@ -1,20 +1,19 @@
 import path from "path";
+import fixEndpointsJs from "express-fix-any-js";
 
-import generateRest from "kschema-fs-api-gen-rest";
-import fixEndpointsJs from "express-fix-endpoints-js";
-
-import { locateSource } from "./InsertGenPk/steps/locateSource.js";
-import { locateDestination } from "./InsertGenPk/steps/locateDestination.js";
+import { locateSource } from "./WithMail/steps/locateSource.js";
+import { locateDestination } from "./WithMail/steps/locateDestination.js";
 import { createFolder } from "../../core/createFolder.js";
 
-import { announce } from "./InsertGenPk/steps/announce.js";
+import updateEndPointsJs from "./WithMail/steps/updateEndPointsJs.js";
+import createHttpFile from "./WithMail/steps/createHttpFile.js";
 
-import resolveFolderName from "./InsertGenPk/steps/resolveFolderName.js";
-import actions from "./InsertGenPk/actions.json" with { type: "json" };
+import { announce } from "./WithMail/steps/announce.js";
 
-const startFunc = async ({ cmd = "", toPath, isAnnounce = true, checkBeforeCreate = true,
-    toConfigPath, inTargetPath
-}) => {
+import resolveFolderName from "./WithMail/steps/resolveFolderName.js";
+import actions from "./WithMail/actions.json" with { type: "json" };
+
+const startFunc = async ({ cmd = "", toPath, isAnnounce = true, checkBeforeCreate = true }) => {
 
     const matched = actions;
 
@@ -43,13 +42,13 @@ const startFunc = async ({ cmd = "", toPath, isAnnounce = true, checkBeforeCreat
 
     if (createFolderResponse.KTF) {
         const fromEndPointsJs = await fixEndpointsJs({
-            endPointsJsPath: path.join(localToPath, "end-points.js"),
-            inActionName: cmd
+            jsFilePath: path.join(localToPath, "end-points.js"),
+            inCheckLines: matched.endPointsJs
         });
 
-        generateRest({
-            toConfigPath, inTargetPath,
-            toPath: path.join(localToPath, resolvedFolderName),
+        createHttpFile({
+            inTargetPath: path.join(localToPath, resolvedFolderName),
+            toPath: process.cwd()
         });
     };
 
