70f35dbae14550a46dcadd53dda966307a602f74
keshavsoft <nknnkumar@yahoo.com>
2026-06-18 16:42:01 +0530

    to test

--- test/v4/InsertGenPk/end-points.js
+++ test/v4/InsertGenPk/end-points.js
@@ -1,13 +1,9 @@
 import express from 'express';
 
-import funcFromKeshavSoftFolderName from './KeshavSoftFolderName/controller.js';
-
 const tableName = "StockItems";
 const tablePath = "Data/StockItems.json";
 const configPath = "Config/Schemas/StockItems.json";
 
 const router = express.Router();
-
-router.post('/insertGenPk', (req, res) => funcFromKeshavSoftFolderName({ req, res, inTablePath: tablePath }));
 
 export { router };
