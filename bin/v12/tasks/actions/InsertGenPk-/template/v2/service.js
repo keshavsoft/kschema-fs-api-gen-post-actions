fd6ca067adfa16366650ade7c188368614538587
keshavsoft <nknnkumar@yahoo.com>
2026-06-22 12:16:05 +0530

    del

--- bin/v12/tasks/actions/InsertGenPk-/template/v2/service.js
+++
@@ -1,9 +0,0 @@
-import insertGenPk from "./insertGenPk.js";
-
-const startFunc = async ({ inRequestBody, inTablePath, inConfigPath }) => {
-    const insertedPk = await insertGenPk({ inRequestBody, inTablePath, inConfigPath });
-
-    return insertedPk;
-};
-
-export { startFunc };
